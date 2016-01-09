dda75db45bf04af57c57bbb6c0a13ff0a9bf2ea9
Kelly de Vries <kdv@me.com>
2016-01-09 11:06:10 -0800

    add more api info to revisit

--- src/index.js
+++ src/index.js
@@ -14,6 +14,22 @@ const twitter = new twitterAPI({
 
 });
 
+twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results) {
+  if (error) {
+    console.log("Error getting OAuth request token : " + error);
+  } else {
+    // store token and tokenSecret somewhere, you'll need them later; redirect user
+  }
+});
+twitter.getAccessToken(requestToken, requestTokenSecret, oauth_verifier, function(error, accessToken, accessTokenSecret, results) {
+  if (error) {
+    console.log(error);
+  } else {
+    // store accessToken and accessTokenSecret somewhere (associated to the user)
+    // verify Credentials belongs here
+  }
+});
+
 TwitterAPI({key: Twitter_API_KEY, term: 'surfboards' }, function(data) {
   console.log(data);
 });
